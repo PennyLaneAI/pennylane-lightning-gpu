c7e954126dff6e4ec8072ab5b6f4fa3250a3e51f
Ali Asadi <ali@xanadu.ai>
2022-06-09 18:45:45 -0400

    Fix the issue with MultiRZ.scaling_factor

--- pennylane_lightning_gpu/src/algorithms/AdjointDiffGPU.hpp
+++ pennylane_lightning_gpu/src/algorithms/AdjointDiffGPU.hpp
@@ -195,8 +195,8 @@ template <class T = double> class AdjointJacobianGPU {
         {"SingleExcitationPlus", -static_cast<T>(0.5)},
         {"DoubleExcitation", -static_cast<T>(0.5)},
         {"DoubleExcitationMinus", -static_cast<T>(0.5)},
-        {"DoubleExcitationPlus", -static_cast<T>(0.5)} {"MultiRZ",
-                                                        static_cast<T>(0.5)}};
+        {"DoubleExcitationPlus", -static_cast<T>(0.5)},
+        {"MultiRZ", static_cast<T>(0.5)}};
 
     /**
      * @brief Utility method to update the Jacobian at a given index by
