cab7ec01d29e2a4ce354bc3c8aa9c1521516207e
Lee J. O'Riordan <lee@xanadu.ai>
2022-10-14 19:42:27 +0000

    Add explicit tests for TensorProdObsGPU

--- pennylane_lightning_gpu/src/tests/Test_ObservablesGPU.cpp
+++ pennylane_lightning_gpu/src/tests/Test_ObservablesGPU.cpp
@@ -77,7 +77,7 @@ TEMPLATE_TEST_CASE("ObservablesGPU::NamedObsGPU", "[ObservablesGPU]", float,
             CHECK(host_array[i].real() == Approx(res2[i].real()));
             CHECK(host_array[i].imag() == Approx(res2[i].imag()));
         }
-        REQUIRE_THROWS(obs3.applyInPlace(sv));
+        CHECK_THROWS(obs3.applyInPlace(sv));
     }
 }
 
@@ -102,16 +102,16 @@ TEMPLATE_TEST_CASE("ObservablesGPU::HermitianObsGPU", "[ObservablesGPU]", float,
     HermitianObsGPU<TestType> obs1{hermitian_h, {0}};
     HermitianObsGPU<TestType> obs2{hermitian_ry, {2}};
 
-    SECTION("NamedObsGPU<TestType> binary ops") {
+    SECTION("HermitianObsGPU<TestType> binary ops") {
         CHECK(obs1 == HermitianObsGPU<TestType>{hermitian_h, {0}});
         CHECK(obs1 != obs2);
     }
-    SECTION("NamedObsGPU<TestType>::getWires") {
+    SECTION("HermitianObsGPU<TestType>::getWires") {
         CHECK(obs1.getWires() == std::vector<size_t>{0});
         CHECK(obs2.getWires() == std::vector<size_t>{2});
     }
 
-    SECTION("NamedObsGPU<TestType>::applyInPlace") {
+    SECTION("HermitianObsGPU<TestType>::applyInPlace") {
         StateVectorCudaManaged<TestType> sv(4);
         sv.initSV();
 
@@ -158,6 +158,99 @@ TEMPLATE_TEST_CASE("ObservablesGPU::HermitianObsGPU", "[ObservablesGPU]", float,
     }
 }
 
+TEMPLATE_TEST_CASE("ObservablesGPU::TensorProdObsGPU", "[ObservablesGPU]",
+                   float, double) {
+    SECTION("TensorProdObsGPU<TestType> CTOR") {
+        // Note: Empty parameter pack will be constructable.
+        REQUIRE(std::is_constructible<TensorProdObsGPU<TestType>>::value);
+        REQUIRE(std::is_constructible<
+                TensorProdObsGPU<TestType>,
+                std::shared_ptr<NamedObsGPU<TestType>>,
+                std::shared_ptr<NamedObsGPU<TestType>>>::value);
+        REQUIRE(std::is_constructible<
+                TensorProdObsGPU<TestType>,
+                std::shared_ptr<NamedObsGPU<TestType>>,
+                std::shared_ptr<HermitianObsGPU<TestType>>>::value);
+        REQUIRE(std::is_constructible<
+                TensorProdObsGPU<TestType>,
+                std::shared_ptr<HermitianObsGPU<TestType>>,
+                std::shared_ptr<HermitianObsGPU<TestType>>>::value);
+    }
+
+    std::vector<std::complex<TestType>> hermitian_h{{0.7071067811865475, 0},
+                                                    {0.7071067811865475, 0},
+                                                    {0.7071067811865475, 0},
+                                                    {-0.7071067811865475, 0}};
+    std::vector<std::complex<TestType>> hermitian_ry{{std::cos(0.1234), 0},
+                                                     {-std::sin(0.1234), 0},
+                                                     {std::sin(0.1234), 0},
+                                                     {std::cos(0.1234), 0}};
+
+    std::shared_ptr<HermitianObsGPU<TestType>> obs1(
+        new HermitianObsGPU<TestType>(hermitian_h, {0}));
+    std::shared_ptr<HermitianObsGPU<TestType>> obs2(
+        new HermitianObsGPU<TestType>(hermitian_ry, {2}));
+    std::shared_ptr<NamedObsGPU<TestType>> obs3(
+        new NamedObsGPU<TestType>("PauliX", {3}, {}));
+    std::shared_ptr<NamedObsGPU<TestType>> obs4(
+        new NamedObsGPU<TestType>("RX", {1}, {0.245}));
+
+    TensorProdObsGPU<TestType> tp_obs1{obs1, obs2};
+    TensorProdObsGPU<TestType> tp_obs2{obs1, obs3};
+    TensorProdObsGPU<TestType> tp_obs3{obs3, obs4};
+
+    SECTION("TensorProdObsGPU<TestType> binary ops") {
+        CHECK(tp_obs1 == TensorProdObsGPU<TestType>{obs1, obs2});
+        CHECK(tp_obs1 != tp_obs2);
+    }
+    SECTION("TensorProdObsGPU<TestType>::getWires") {
+        CHECK(tp_obs1.getWires() == std::vector<size_t>{0, 2});
+        CHECK(tp_obs2.getWires() == std::vector<size_t>{0, 3});
+        CHECK(tp_obs3.getWires() == std::vector<size_t>{1, 3});
+        CHECK_THROWS(TensorProdObsGPU<TestType>{obs1, obs1});
+    }
+
+    SECTION("TensorProdObsGPU<TestType>::applyInPlace") {
+        StateVectorCudaManaged<TestType> sv(4);
+        sv.initSV();
+        sv.applyHadamard({1}, false);
+        sv.applyHadamard({2}, false);
+
+        std::vector<std::complex<TestType>> host_array(16, {0, 0});
+        std::vector<std::complex<TestType>> res1 = {
+            {0.30734708, 0}, {0, 0}, {0.39438277, 0}, {0, 0},
+            {0.30734708, 0}, {0, 0}, {0.39438277, 0}, {0, 0},
+            {0.30734708, 0}, {0, 0}, {0.39438277, 0}, {0, 0},
+            {0.30734708, 0}, {0, 0}, {0.39438277, 0}, {0, 0}};
+
+        tp_obs1.applyInPlace(sv);
+        sv.getDataBuffer().CopyGpuDataToHost(host_array.data(),
+                                             host_array.size());
+        for (std::size_t i = 0; i < host_array.size(); i++) {
+            CHECK(host_array[i].real() == Approx(res1[i].real()));
+            CHECK(host_array[i].imag() == Approx(res1[i].imag()));
+        }
+
+        std::vector<std::complex<TestType>> res2 = {
+            {0, 0}, {0.3050439, -0.03755592},
+            {0, 0}, {0.39142737, -0.04819115},
+            {0, 0}, {0.3050439, -0.03755592},
+            {0, 0}, {0.39142737, -0.04819115},
+            {0, 0}, {0.3050439, -0.03755592},
+            {0, 0}, {0.39142737, -0.04819115},
+            {0, 0}, {0.3050439, -0.03755592},
+            {0, 0}, {0.39142737, -0.04819115}};
+
+        tp_obs3.applyInPlace(sv);
+        sv.getDataBuffer().CopyGpuDataToHost(host_array.data(),
+                                             host_array.size());
+        for (std::size_t i = 0; i < host_array.size(); i++) {
+            CHECK(host_array[i].real() == Approx(res2[i].real()));
+            CHECK(host_array[i].imag() == Approx(res2[i].imag()));
+        }
+    }
+}
+
 TEMPLATE_TEST_CASE("ObservablesGPU::HamiltonianGPU", "[ObservablesGPU]", float,
                    double) {
     SECTION("HamiltonianGPU<TestType> CTOR") {
@@ -165,15 +258,5 @@ TEMPLATE_TEST_CASE("ObservablesGPU::HamiltonianGPU", "[ObservablesGPU]", float,
         REQUIRE(std::is_constructible<
                 HamiltonianGPU<TestType>, std::vector<TestType>,
                 std::vector<std::shared_ptr<ObservableGPU<TestType>>>>::value);
-    }
-}
-
-TEMPLATE_TEST_CASE("ObservablesGPU::TensorProdObsGPU", "[ObservablesGPU]",
-                   float, double) {
-    SECTION("TensorProdObsGPU<TestType> CTOR") {
-        // Note: detecting empty parameter pack not trivially implementable
-        REQUIRE(std::is_constructible<TensorProdObsGPU<TestType>,
-                                      HermitianObsGPU<TestType>,
-                                      NamedObsGPU<TestType>>::value);
     }
 }
