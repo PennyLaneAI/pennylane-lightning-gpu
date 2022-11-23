644a300769ffc7a152dac009fea830d739eabed5
AmintorDusko <amintor_dusko@hotmail.com>
2022-11-23 15:43:48 -0500

    fix typo

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Integration tests for the ``execute`` method of LightningQubit.
+Integration tests for the ``execute`` method of LightningGPU.
 """
 import pytest
 
