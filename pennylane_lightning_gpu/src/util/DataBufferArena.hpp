0e145d22e1b02b67396fe0c1c6e635bc940518d3
Lee J. O'Riordan <loriordan@gmail.com>
2023-03-15 18:01:04 -0400

    Fix function call signatures and internal structural failings

--- pennylane_lightning_gpu/src/util/DataBufferArena.hpp
+++ pennylane_lightning_gpu/src/util/DataBufferArena.hpp
@@ -7,20 +7,8 @@
 namespace Pennylane::CUDA {
 
 template <class GPUDataT, class DevTagT = int> class DataBufferArena {
-  private:
-    std::vector<std::size_t> lengths_;
-    std::size_t total_length_;
-    std::vector<std::size_t> gpu_buffer_offsets_;
-    DevTag<DevTagT> dev_tag_;
-    GPUDataT *gpu_buffer_begin_;
-    GPUDataT *gpu_buffer_end_;
-
   public:
     using type = GPUDataT;
-
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    ///////////////////////////////////////////////////////////////////////////
 
     DataBufferArena(const std::vector<std::size_t> &buffer_lengths,
                     int device_id = 0, cudaStream_t stream_id = 0,
@@ -36,7 +24,7 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
             gpu_buffer_offsets_[0] = 0;
             // Define the pointer offsets for each buffer
             std::partial_sum(buffer_lengths.cbegin(), buffer_lengths.cend(),
-                             buffer_lengths.begin() + 1);
+                             gpu_buffer_offsets_.begin() + 1);
             total_length_ = gpu_buffer_offsets_.back();
             // Allocate the total buffer size worth of GPU memory
             PL_CUDA_IS_SUCCESS(
@@ -56,7 +44,7 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
             gpu_buffer_offsets_.reserve(buffer_lengths.size() + 1);
             gpu_buffer_offsets_[0] = 0;
             std::partial_sum(buffer_lengths.cbegin(), buffer_lengths.cend(),
-                             buffer_lengths.begin() + 1);
+                             gpu_buffer_offsets_.begin() + 1);
             total_length_ = gpu_buffer_offsets_.back();
 
             PL_CUDA_IS_SUCCESS(
@@ -75,7 +63,7 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
             gpu_buffer_offsets_.reserve(buffer_lengths.size() + 1);
             gpu_buffer_offsets_[0] = 0;
             std::partial_sum(buffer_lengths.cbegin(), buffer_lengths.cend(),
-                             buffer_lengths.begin() + 1);
+                             gpu_buffer_offsets_.begin() + 1);
             total_length_ = gpu_buffer_offsets_.back();
 
             PL_CUDA_IS_SUCCESS(
@@ -84,10 +72,6 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
             gpu_buffer_end_ = gpu_buffer_begin_ + total_length_;
         }
     }
-
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    ///////////////////////////////////////////////////////////////////////////
 
     // Buffer should never be default initialized
     DataBufferArena() = delete;
@@ -155,13 +139,39 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
         }
     };
 
-    auto getData() -> GPUDataT * { return gpu_buffer_begin_; }
-    auto getData() const -> const GPUDataT * { return gpu_buffer_begin_; }
+    /**
+     * @brief Get mutable const buffer pointer for the arena buffer.
+     *
+     * @return GPUDataT*
+     */
+    inline auto getData() -> GPUDataT * { return gpu_buffer_begin_; }
 
-    auto getData(std::size_t index) -> GPUDataT * {
+    /**
+     * @brief Get the const buffer pointer for the arena buffer.
+     *
+     * @return const GPUDataT*
+     */
+    inline auto getData() const -> const GPUDataT * {
+        return gpu_buffer_begin_;
+    }
+
+    /**
+     * @brief Get the mutable buffer pointer for the region at the given index.
+     *
+     * @param index Buffer region index.
+     * @return GPUDataT*
+     */
+    inline auto getData(std::size_t index) -> GPUDataT * {
         return gpu_buffer_begin_ + gpu_buffer_offsets_[index];
     }
-    auto getData(std::size_t buffer_index) const -> const GPUDataT * {
+
+    /**
+     * @brief Get the const buffer pointer for the region at the given index.
+     *
+     * @param index Buffer region index.
+     * @return const GPUDataT*
+     */
+    inline auto getData(std::size_t index) const -> const GPUDataT * {
         return gpu_buffer_begin_ + gpu_buffer_offsets_[index];
     }
 
@@ -173,17 +183,53 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
         PL_CUDA_IS_SUCCESS(
             cudaMemset(gpu_buffer_begin_, 0, total_length_ * sizeof(GPUDataT)));
     }
+
+    /**
+     * @brief Zero-initialize the GPU buffer region at the given index.
+     *
+     * @param index Buffer region to zero.
+     */
     void zeroInit(std::size_t index) {
         PL_CUDA_IS_SUCCESS(
             cudaMemset(getData(index), 0, lengths_[index] * sizeof(GPUDataT)));
     }
 
-    auto getLength() const { return total_length_; }
-    auto getLength(std::size_t index) const { return lengths_[index]; }
+    /**
+     * @brief Get the total length of the arena buffer, including all
+     * partitions.
+     *
+     * @return auto
+     */
+    inline auto getLength() const { return total_length_; }
 
-    auto &getBufferLengths() const { return lengths_; }
-    auto getNumBuffers() const { return lengths_.size(); }
-    auto &getBufferOffsets() const { return gpu_buffer_offsets_; }
+    /**
+     * @brief Get the length of the arena buffer at the given index.
+     *
+     * @param index Arena buffer region index.
+     * @return std::size_t
+     */
+    inline auto getLength(std::size_t index) const { return lengths_[index]; }
+
+    /**
+     * @brief Get the length of all arena regions.
+     *
+     * @return const std::vector<std::size_t>&
+     */
+    const auto &getBufferLengths() const { return lengths_; }
+
+    /**
+     * @brief Get the number of arena partitions.
+     *
+     * @return std:size_t
+     */
+    inline auto getNumBuffers() const { return lengths_.size(); }
+
+    /**
+     * @brief Get the offsets into each arena region indexed from front to back.
+     *
+     * @return const std::vector<std::size_t>&
+     */
+    const auto &getBufferOffsets() const { return gpu_buffer_offsets_; }
 
     /**
      * @brief Get the CUDA stream for the given object.
@@ -195,32 +241,17 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
     }
 
     inline auto getDevice() const -> int { return dev_tag_.getDeviceID(); }
-
     inline auto getDevTag() const -> const DevTag<DevTagT> & {
         return dev_tag_;
     }
 
     /**
-     * @brief Copy data from another GPU memory block to here.
+     * @brief Copy data of given length from GPU to this GPU.
      *
+     * @param gpu_in GPU data pointer to copy from.
+     * @param length Length of buffer region.
+     * @param async Use asynchronous copy. Defaults to false.
      */
-    void CopyGpuDataToGpu(const GPUDataT *gpu_in, std::size_t length,
-                          bool async = false, std::size_t index = 0) {
-        PL_ABORT_IF_NOT(
-            getLength(index) == length,
-            "Sizes do not match for GPU data. Please ensure the source "
-            "buffer is not larger than the destination buffer");
-        if (async) {
-            PL_CUDA_IS_SUCCESS(cudaMemcpyAsync(
-                getData(index), gpu_in, sizeof(GPUDataT) * length,
-                cudaMemcpyDeviceToDevice, getStream()));
-        } else {
-            PL_CUDA_IS_SUCCESS(cudaMemcpy(getData(index), gpu_in,
-                                          sizeof(GPUDataT) * length,
-                                          cudaMemcpyDefault));
-        }
-    }
-
     void CopyGpuDataToGpu(const GPUDataT *gpu_in, std::size_t length,
                           bool async = false) {
         PL_ABORT_IF_NOT(
@@ -239,21 +270,64 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
     }
 
     /**
-     * @brief Copy data from another GPU memory block to here.
+     * @brief Copy data of given length from GPU to this GPU starting at the
+     * given arena index.
      *
+     * @param gpu_in GPU data pointer to copy from.
+     * @param length Length of buffer region.
+     * @param async Use asynchronous copy. Defaults to false.
+     * @param index Index into arena partitions.
      */
-    void CopyGpuDataToGpu(const DataBufferArena &buffer, bool async = false) {
-        CopyGpuDataToGpu(buffer.getData(), buffer.getLengthTotal(), async);
-    }
-
-    void CopyGpuDataToGpu(const DataBufferArena &buffer, bool async = false,
-                          std::size_t index = 0) {
-        CopyGpuDataToGpu(buffer.getData(index), buffer.getLength(index), async);
+    void CopyGpuDataToGpu(const GPUDataT *gpu_in, std::size_t length,
+                          bool async, std::size_t index) {
+        PL_ABORT_IF_NOT(
+            getLength(index) == length,
+            "Sizes do not match for GPU data. Please ensure the source "
+            "buffer is not larger than the destination buffer");
+        if (async) {
+            PL_CUDA_IS_SUCCESS(cudaMemcpyAsync(
+                getData(index), gpu_in, sizeof(GPUDataT) * length,
+                cudaMemcpyDeviceToDevice, getStream()));
+        } else {
+            PL_CUDA_IS_SUCCESS(cudaMemcpy(getData(index), gpu_in,
+                                          sizeof(GPUDataT) * length,
+                                          cudaMemcpyDefault));
+        }
     }
 
     /**
-     * @brief Explicitly copy data from host memory to GPU device.
+     * @brief Copy another DataBufferArena to this GPU.
      *
+     * @param buffer Other arena buffer to copy.
+     * @param async Use asynchronous copy. Defaults to false.
+     */
+    void CopyGpuDataToGpu(const DataBufferArena &buffer, bool async = false) {
+        CopyGpuDataToGpu(buffer.getData(), buffer.getLength(), async);
+    }
+
+    /**
+     * @brief Copy another DataBufferArena indexed partition to this GPU at the
+     * given index.
+     *
+     * @param buffer Other arena buffer to copy.
+     * @param async Use asynchronous copy. Defaults to false.
+     * @param src_index Index into source buffer.
+     * @param tgt_index Index into target buffer.
+     */
+    void CopyGpuDataToGpu(const DataBufferArena &buffer, bool async,
+                          std::size_t src_index, std::size_t tgt_index) {
+        CopyGpuDataToGpu(buffer.getData(src_index), buffer.getLength(src_index),
+                         async, tgt_index);
+    }
+
+    /**
+     * @brief Copy data of given length from host memory to GPU.
+     *
+     * @tparam HostDataT Host data type. Defaults to be GPUDataT. Aborts if type
+     * sizes do not match.
+     * @param host_in Host data pointer to copy from.
+     * @param length Length of buffer region.
+     * @param async Use asynchronous copy. Defaults to false.
      */
     template <class HostDataT = GPUDataT>
     void CopyHostDataToGpu(const HostDataT *host_in, std::size_t length,
@@ -273,27 +347,44 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
         }
     }
 
+    /**
+     * @brief Copy data of given length from host memory to GPU starting at the
+     * given arena index.
+     *
+     * @tparam HostDataT Host data type. Defaults to be GPUDataT. Aborts if type
+     * sizes do not match.
+     * @param host_in Host input data buffer.
+     * @param length Length of host buffer region.
+     * @param async Use asynchronous copy. Defaults to false.
+     * @param tgt_index Index into GPU target buffer.
+     */
     template <class HostDataT = GPUDataT>
     void CopyHostDataToGpu(const HostDataT *host_in, std::size_t length,
-                           bool async = false, std::size_t index = 0) {
+                           bool async, std::size_t tgt_index) {
         PL_ABORT_IF_NOT(
             (getLength() * sizeof(GPUDataT)) == (length * sizeof(HostDataT)),
             "Sizes do not match for host & GPU data. Please ensure the source "
             "buffer is not larger than the destination buffer");
         if (async) {
-            PL_CUDA_IS_SUCCESS(cudaMemcpyAsync(
-                getData(index), host_in, sizeof(GPUDataT) * getLength(index),
-                cudaMemcpyHostToDevice, getStream()));
+            PL_CUDA_IS_SUCCESS(
+                cudaMemcpyAsync(getData(tgt_index), host_in,
+                                sizeof(GPUDataT) * getLength(tgt_index),
+                                cudaMemcpyHostToDevice, getStream()));
         } else {
-            PL_CUDA_IS_SUCCESS(cudaMemcpy(getData(index), host_in,
-                                          sizeof(GPUDataT) * getLength(index),
-                                          cudaMemcpyDefault));
+            PL_CUDA_IS_SUCCESS(cudaMemcpy(
+                getData(tgt_index), host_in,
+                sizeof(GPUDataT) * getLength(tgt_index), cudaMemcpyDefault));
         }
     }
 
     /**
-     * @brief Explicitly copy data from GPU device to host memory.
+     * @brief Copy data of given length from GPU buffer to host.
      *
+     * @tparam HostDataT Host data type. Defaults to be GPUDataT. Aborts if type
+     * sizes do not match.
+     * @param host_out Host output data buffer.
+     * @param length Length of host buffer region.
+     * @param async Use asynchronous copy. Defaults to false.
      */
     template <class HostDataT = GPUDataT>
     inline void CopyGpuDataToHost(HostDataT *host_out, std::size_t length,
@@ -312,24 +403,43 @@ template <class GPUDataT, class DevTagT = int> class DataBufferArena {
                 cudaMemcpyDeviceToHost, getStream()));
         }
     }
+
+    /**
+     * @brief Copy data of given length from GPU buffer to host.
+     *
+     * @tparam HostDataT Host data type. Defaults to be GPUDataT. Aborts if type
+     * sizes do not match.
+     * @param host_out Host output data buffer.
+     * @param length Length of host buffer region.
+     * @param async Use asynchronous copy. Defaults to false.
+     * @param src_index GPU arena buffer source index.
+     */
     template <class HostDataT = GPUDataT>
     inline void CopyGpuDataToHost(HostDataT *host_out, std::size_t length,
-                                  bool async = false,
-                                  std::size_t index = 0) const {
+                                  bool async, std::size_t src_index) const {
         PL_ABORT_IF_NOT(
-            (getLength(index) * sizeof(GPUDataT)) ==
+            (getLength(src_index) * sizeof(GPUDataT)) ==
                 (length * sizeof(HostDataT)),
             "Sizes do not match for host & GPU data. Please ensure the source "
             "buffer is not larger than the destination buffer");
         if (!async) {
-            PL_CUDA_IS_SUCCESS(cudaMemcpy(host_out, getData(index),
-                                          sizeof(GPUDataT) * getLength(index),
-                                          cudaMemcpyDefault));
+            PL_CUDA_IS_SUCCESS(cudaMemcpy(
+                host_out, getData(src_index),
+                sizeof(GPUDataT) * getLength(src_index), cudaMemcpyDefault));
         } else {
-            PL_CUDA_IS_SUCCESS(cudaMemcpyAsync(
-                host_out, getData(index), sizeof(GPUDataT) * getLength(index),
-                cudaMemcpyDeviceToHost, getStream()));
+            PL_CUDA_IS_SUCCESS(
+                cudaMemcpyAsync(host_out, getData(src_index),
+                                sizeof(GPUDataT) * getLength(src_index),
+                                cudaMemcpyDeviceToHost, getStream()));
         }
     }
+
+  private:
+    std::vector<std::size_t> lengths_;
+    std::size_t total_length_;
+    std::vector<std::size_t> gpu_buffer_offsets_;
+    DevTag<DevTagT> dev_tag_;
+    GPUDataT *gpu_buffer_begin_;
+    GPUDataT *gpu_buffer_end_;
 };
 } // namespace Pennylane::CUDA
