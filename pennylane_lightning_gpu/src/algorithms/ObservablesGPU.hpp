7a6681203b41a5912805b4f5b46bad67da25b3f7
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-03-23 13:15:27 -0400

    Update pennylane_lightning_gpu/src/algorithms/ObservablesGPU.hpp
    Co-authored-by: Lee James O'Riordan <mlxd@users.noreply.github.com>

--- pennylane_lightning_gpu/src/algorithms/ObservablesGPU.hpp
+++ pennylane_lightning_gpu/src/algorithms/ObservablesGPU.hpp
@@ -562,7 +562,7 @@ class SparseHamiltonianGPU final : public ObservableGPU<T> {
             /* cusparseDnVecDescr_t */ sv_prime_descriptor,
             /* cudaDataType */ data_type,
             /* cusparseSpMVAlg_t */
-            CUSPARSE_SPMV_ALG_DEFAULT,
+            CUSPARSE_SPMV_CSR_ALG1,
             /* void* */ reinterpret_cast<void *>(dBuffer.getData())));
 
         // destroy matrix/vector descriptors
