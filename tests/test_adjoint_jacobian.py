7ac0ed8718b443b7bbc2e1ff6b70eb28573acb3f
Shuli Shu <08cnbj@gmail.com>
2023-05-05 20:08:55 +0000

    update

--- tests/test_adjoint_jacobian.py
+++ tests/test_adjoint_jacobian.py
@@ -1010,21 +1010,21 @@ def test_fail_adjoint_mixed_Hamiltonian_Hermitian(returns):
             #qml.utils.sparse_hamiltonian(
             qml.Hamiltonian(
                 0.1 * qml.PauliX(wires=custom_wires[0]) @ qml.PauliZ(wires=custom_wires[1])
-            ).sparse_matrix(),
+            ).sparse_matrix(custom_wires),
             wires=custom_wires,
         ),
         qml.SparseHamiltonian(
             #qml.utils.sparse_hamiltonian(
             qml.Hamiltonian(
                 2 * qml.PauliX(wires=custom_wires[2]) @ qml.PauliZ(wires=custom_wires[0])
-            ).sparse_matrix(),
+            ).sparse_matrix(custom_wires),
             wires=custom_wires,
         ),
         qml.SparseHamiltonian(
             #qml.utils.sparse_hamiltonian(
             qml.Hamiltonian(
                 1.1 * qml.PauliX(wires=custom_wires[0]) @ qml.PauliZ(wires=custom_wires[2])
-            ).sparse_matrix(),
+            ).sparse_matrix(custom_wires),
             wires=custom_wires,
         ),
     ],
